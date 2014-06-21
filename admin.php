f209f3897fe879c462e1e59d698f4787b3836063
unknown <vbnm123c@gmail.com>
2014-06-21 21:42:28 +0800

    admin display html by php

--- admin.php
+++ admin.php
@@ -4,36 +4,33 @@
     session_start();
 
     if(isset($_SESSION['access']) && $_SESSION['access']==true) {
-        $admin_access=true;
+        $_SESSION['admin']=true;
+
+        echo "<html>
+                <head>
+                    <meta http-equiv=\"Content-Type\" content=\"charset=utf-8\">
+                    <title>Noods - Admin Page</title>
+                    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/admin.css\">
+                </head>
+                <body>
+                    <div id='functions'>
+                        <ul>
+                          <li>新增店家</li>
+                          <li><a href='search.php'>查詢店家訂單</a></li>
+                          <li>列出所有訂單</li>
+                          <li>修改訂單</li>
+                          <li>刪除訂單</li>
+                        </ul>
+                    </div>
+                </body>
+            </html>";
+
+
+
+        mysqli_close($con);
     } else {
         echo "You shall not pass!";
         mysqli_close($con);
         header('Refresh: 3; url=index.php');
     }
-?>
-
-
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="charset=utf-8">
-    <title>Noods - Admin Page</title>
-    <link rel="stylesheet" type="text/css" href="css/admin.css">
-
-    </head>
-    <body>
-        <div id='functions'>
-            <ul>
-              <li>新增店家</li>
-              <li>查詢店家訂單</li>
-              <li>列出所有訂單</li>
-              <li>修改</li>
-              <li>刪除訂單</li>
-            </ul>
-        </div>
-    </body>
-</html>
-
-
-<?php
-    mysqli_close($con);
 ?>
