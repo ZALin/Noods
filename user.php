2e6a4c39df2850eab62bb17ec86a4c45a5c463d7
unknown <vbnm123c@gmail.com>
2014-06-23 03:15:18 +0800

    user page update

--- user.php
+++ user.php
@@ -4,27 +4,24 @@
     session_start();
 
     if(isset($_SESSION['access']) && $_SESSION['access']==true) {
-        $_SESSION['admin']=true;
 
         echo "<html>
                 <head>
-                    <meta http-equiv=\"Content-Type\" content=\"charset=utf-8\">
+                    <meta http-equiv='Content-Type' content='charset=utf-8'>
                     <title>Noods - User Page</title>
-                    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/admin.css\">
+                    <link rel='stylesheet' type='text/css' href='css/admin.css'>
                 </head>
                 <body>
                     <div id='functions'>
                         <ul>
                           <li><a href='search.php'>查詢店家訂單</a></li>
-                          <li>列出所有訂單</li>
+                          <li><a href='listall.php'>列出所有訂單</a></li>
                           <li>修改訂單</li>
                           <li>刪除訂單</li>
                         </ul>
                     </div>
                 </body>
             </html>";
-
-
 
         mysqli_close($con);
     } else {
