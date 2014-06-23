bf0318884799d8e7f54165c633427bc1a2376fab
Zhe-An Lin <andy03031193@gmail.com>
2014-06-24 00:55:12 +0800

    calculate totalcost

--- upd.php
+++ upd.php
@@ -10,9 +10,24 @@
         $stmt = mysqli_prepare($con,"UPDATE `Subscribe` SET `productNum` = ? WHERE `orderID` = ? AND `productID` = ?");
         mysqli_stmt_bind_param($stmt,'sss',$num, $oid, $pid);
         mysqli_stmt_execute($stmt);
+        
+        //calculate totalCost
+        $stmt = mysqli_prepare($con,"SELECT * FROM `Subscribe` NATURAL JOIN `Product` WHERE `orderID` = ?");
+        mysqli_stmt_bind_param($stmt,'s',$oid);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        mysqli_stmt_bind_result($stmt, $res_productID, $res_subscribeID, $res_orderID, $res_pruductNum, $res_productClass, $res_productName, $res_productCost);
+        $total_cost = 0;
+        while(mysqli_stmt_fetch($stmt)) {
+            $total_cost += $res_pruductNum*$res_productCost;
+        }      
+        $stmt = mysqli_prepare($con,"UPDATE `Order` SET `totalCost` = ? WHERE `orderID` = ? ");
+        mysqli_stmt_bind_param($stmt,'ss',$total_cost, $oid);
+        mysqli_stmt_execute($stmt);
+        
         echo "Update!";
-        //to do  calculate totalCost
-        header('Refresh: 1; url=UpdatePage.php');
+        mysqli_close($con);
+        header("Refresh: 1; url=updateorder.php");
     }
     else{
         echo "You shall not pass!";
