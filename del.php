2d73f3a9dc4b1e1bacdcac7e62f6aa3fb3b99859
Zhe-An Lin <andy03031193@gmail.com>
2014-06-24 00:14:04 +0800

    delete can delete subscribe

--- del.php
+++ del.php
@@ -9,6 +9,9 @@
         $stmt = mysqli_prepare($con,"DELETE FROM `Order` WHERE `orderID` = ?");
         mysqli_stmt_bind_param($stmt,'s',$del_orderid);
         mysqli_stmt_execute($stmt);
+        $stmt = mysqli_prepare($con,"DELETE FROM `Subscribe` WHERE `orderID` = ?");
+        mysqli_stmt_bind_param($stmt,'s',$del_orderid);
+        mysqli_stmt_execute($stmt);
         mysqli_close($con);
         header('Refresh: 1; url=delorder.php');
     }
