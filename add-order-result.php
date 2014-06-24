e2dd32cdb1121fa32a6f2973f0c9088d141554a9
unknown <vbnm123c@gmail.com>
2014-06-24 23:38:01 +0800

    fix add order that the id of order

--- add-order-result.php
+++ add-order-result.php
@@ -4,17 +4,41 @@
     session_start();
     
     if(isset($_SESSION['permission']) && $_SESSION['permission']=='user' && isset($_SESSION['access']) && $_SESSION['access']==true) {
-        $add_orderid=$_POST['orderID'];
         $add_orderdate=$_POST['orderDate'];
+        $add_date=explode("-", $add_orderdate);
+        $add_shopID=$_SESSION['shopID'];
+
+        $stmt = mysqli_prepare($con,'SELECT `orderID` FROM `Order` WHERE `shopID` = ? AND orderDate = ?');
+        mysqli_stmt_bind_param($stmt,'is',$add_shopID,$add_orderdate);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        mysqli_stmt_bind_result($stmt, $res_orderID);
+        while(mysqli_stmt_fetch($stmt)) {
+            //echo $res_orderID . "<br>";
+        }
+
+        $nrow=mysqli_stmt_num_rows($stmt);
+
+        mysqli_stmt_close($stmt);
+
+        if($nrow<10){
+            $nrow = "0" . $nrow;
+        }
+        if($add_shopID<10) {
+            $add_shopID = "00" .$add_shopID;
+        } else if ($add_shopID < 99){
+            $add_shopID = "0" .$add_shopID;
+        }
+
+
+        $add_orderid = $add_shopID."-".$add_date[0][2].$add_date[0][3].$add_date[1].$add_date[2]."-".$nrow;
 
         $stmt = mysqli_prepare($con,"INSERT INTO `Order` (`orderID`, `orderDate`, `shopID`, `totalCost`) VALUES ( ? , ? , ?, 0)");
-
-        mysqli_stmt_bind_param($stmt,'sss',$add_orderid,$add_orderdate,$_SESSION['shopID']);
+        mysqli_stmt_bind_param($stmt,'ssi',$add_orderid,$add_orderdate,$_SESSION['shopID']);
 
         if(mysqli_stmt_execute($stmt)) {
 
             echo $add_orderid . " 已經成功加入資料庫 !!<br>Redirecting...";
-            
 
         } else {
 
--- add-order.php
+++ add-order.php
@@ -15,39 +15,14 @@
                 <link rel='stylesheet' href='css/ui-lightness/jquery-ui-1.10.4.custom.css'>
                 <script>
                 
-
                 $(function() {
-
-                    function pad (str, max) {
-                        str = str.toString();
-                        return str.length < max ? pad("0" + str, max) : str;
-                    }
 
                     $('#datepicker').datepicker();
                     $('#datepicker').on('keyup mouseup change', function () {
                         var date=$('#datepicker').val().split('/');
-                        var year=parseInt(date[2]);
-                        var month=parseInt(date[0]);
-                        var day=parseInt(date[1]);
-                        var sid=<?php echo $_SESSION['shopID'];?>;
-                        sid=pad(sid,3);
-                        var smid=((year%100)*10000+month*100+day).toString();
 
                         var phpdate=[date[2],date[0],date[1]].join('-');
-                        document.cookie = 'dt='+phpdate; 
-
-                        <?php
-                            $stmt = mysqli_prepare($con,'SELECT `shopID` FROM `Order` WHERE `shopID` = ? and orderDate = ?');
-                            mysqli_bind_param($stmt,'is',$_SESSION['shopID'],$_COOKIE['dt']);
-                            mysqli_stmt_execute($stmt);
-                            mysqli_stmt_store_result($stmt);
-                            mysqli_stmt_bind_result($stmt,$res_shopID);
-                            echo "var slast=" .mysqli_stmt_num_rows($stmt) . ";";
-                        ?>
-                        slast=pad(slast,2);
-                        var idstring=[sid,smid,slast].join('-');
                         $('#datelabel').val(phpdate);
-                        $('#idlabel').val(idstring);
                     });
 
                 });
@@ -55,9 +30,9 @@
                 </script>
             </head>
             <body>
+                
                 <p>Date: <input type='text' id='datepicker'> </p>
                 <form action='add-order-result.php' method='post'>
-                    <input id='idlabel' name='orderID' type='hidden'></input><br>
                     <input id='datelabel' name='orderDate' type='hidden'></input><br>
                     <input type='submit' value='新增'>
                 </form>
