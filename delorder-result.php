e45bd7f33f0bc5a94a73bb06eb792cec06e6e8f7
unknown <vbnm123c@gmail.com>
2014-06-24 22:17:51 +0800

    fix remain echo

--- delorder-result.php
+++ delorder-result.php
@@ -4,7 +4,7 @@
     session_start();
     
     if(isset($_SESSION['access']) && $_SESSION['access']==true) {
-        echo "delete!";
+
         $del_orderid=$_GET['id'];
         $stmt = mysqli_prepare($con,"DELETE FROM `Order` WHERE `orderID` = ?");
         mysqli_stmt_bind_param($stmt,'s',$del_orderid);
