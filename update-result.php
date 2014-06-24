d7e757c93f63e0311b93ce553cc0130103e74b05
Zhe-An Lin <andy03031193@gmail.com>
2014-06-24 21:26:13 +0800

    fix update-result.php can delete when productNum is 0

--- update-result.php
+++ update-result.php
@@ -7,9 +7,16 @@
         $order_id=$_POST['oid'];
         $product_id=$_POST['pid'];
         $product_num=$_POST['num'];
-        $stmt = mysqli_prepare($con,"UPDATE `Subscribe` SET `productNum` = ? WHERE `orderID` = ? AND `productID` = ?");
-        mysqli_stmt_bind_param($stmt,'sss',$product_num, $order_id, $product_id);
-        $upd_1=mysqli_stmt_execute($stmt);
+        if( intval($product_num) === 0){
+            $stmt = mysqli_prepare($con,"DELETE FROM `Subscribe` WHERE `orderID` = ? AND `productID` = ?");
+            mysqli_stmt_bind_param($stmt,'si',$order_id, $product_id);
+            $upd_1=mysqli_stmt_execute($stmt);
+        }
+        else{
+            $stmt = mysqli_prepare($con,"UPDATE `Subscribe` SET `productNum` = ? WHERE `orderID` = ? AND `productID` = ?");
+            mysqli_stmt_bind_param($stmt,'isi',$product_num, $order_id, $product_id);
+            $upd_1=mysqli_stmt_execute($stmt);
+        }
         
         //calculate totalCost
         $stmt = mysqli_prepare($con,"SELECT `productNum`,`productCost` FROM `Subscribe` NATURAL JOIN `Product` WHERE `orderID` = ?");
