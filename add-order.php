a3e011fb5c6698956253073905bda45777b86c14
unknown <vbnm123c@gmail.com>
2014-06-24 13:18:33 +0800

    add ui js css

--- add-order.php
+++ add-order.php
@@ -9,6 +9,10 @@
                 <head>
                     <meta http-equiv='Content-Type' content='charset=utf-8'>
                     <title>Noods - Add-Order Page</title>
+                    <script src='js/jquery-1.10.2.js'></script>
+                    <script src='js/jquery-ui-1.10.4.custom.js'></script>
+
+                    <link href='css/ui-lightness/jquery-ui-1.10.4.custom.css' rel='stylesheet'>
                     <link rel='stylesheet' type='text/css' href='css/add-order.css'>
                 </head>
                 <body>
